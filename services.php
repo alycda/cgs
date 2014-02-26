921bc626e0ef696a822ba96ef3b7d77c11a4831c
Alyssa Davis <alyda@me.com>
2014-02-26 00:52:05 -0800

    content: services

---
+++ services.php
@@ -0,0 +1,101 @@
+<?php
+$pageTitle = 'Services';
+include 'inc/_header.php'; ?>
+<ul class="breadcrumb">
+	<li><a href="/">home</a></li>
+	<li>services</li>
+</ul>
+
+<!-- use .md files to edit -->
+<section class="jumbotron">
+	<header>
+	<h1>Quality Aircraft Component Repair</h1>
+	</header>
+
+	<div class="row">
+		<article class="col-xs-6">
+			<h2><a name="oxygen-bottles">Oxygen Bottles</a></h2>
+			<img src="images/oxygen.png" alt="Oxygen Bottles" class="pull-right">
+			<p class="small">CGS has the capability to hydro test, overhaul, and repair almost any portable or fixed-system oxygen bottle. Every hydro test includes a complete regulator functional test, leak check, and cylinder purge to ensure the unit operates within manufacturer specifications and arrives ready for installation. Don’t settle for shops that only hydro test cylinders when CGS can offer full-service 8130-3 recertification.</p>
+		</article>
+
+		<article class="col-xs-6">
+			<h2><a name="fire">Fire Bottles &amp; Extinguishers</a></h2>
+			<span class="pull-right"><img src="images/fixed.png" alt="Fixed-System Fire Extinguishers"><br/><img src="images/fire.png" alt="Fixed-System Fire Extinguishers"></span>
+			<p class="small">At CGS, experienced personnel can overhaul the most common fire bottles found in corporate and commercial aviation at a fraction of the price of the OEM. You can save thousands utilizing our network of authorized PMA parts suppliers and reasonable service rates. Prefer OEM parts? We have you covered!</p><br/>
+			<p class="small">CGS also sells and services Halon 1211 and liquid fire extinguishers for use in the cockpit and cabin. We have parts in stock and we carry affordable, FAA-approved replacement extinguishers.</p>
+		</article>
+
+	</div>
+
+	<div class="row">
+		<article class="col-xs-6">
+			<h2><a name="pneumatic-bottles">Pneumatic Bottles</a></h2>
+			<img src="images/pneumatic.png" alt="Pneumatic Bottles" class="pull-right">
+			<p class="small">All types of blow down, life raft, and other pneumatic reservoirs can be hydro tested quickly and inexpensively. We can also perform x-ray inspections. No need for costly exchanges!</p>
+		</article>
+
+		<h1 hidden>Parts</h1>
+		<article class="col-xs-6">
+			<h2><a name="parts">Parts</a></h2>
+			<img src="images/parts.png" alt="Parts" class="pull-right">
+			<p class="small">We have an extensive inventory of aircraft components, including oxygen cylinders, portable fire extinguishers, fire bottles, and regulators. We also carry compressed gas-related ground support equipment such as high pressure lines and fittings, oxygen fill adaptors, and booster pumps.</p>
+		</article>
+	</div>
+
+	<hr/>
+	<header>
+	<h1>Hydrostatic Testing</h1>
+	<p class="small text-center">DOT Approved Hydrostatic Testing Service.</p>
+	</header>
+	<br/>
+
+
+	<p>$25.00 flat rate for industrial, medical, SCUBA, &amp; paintball testing includes valve removal and installation. We also provide complimentary O-ring replacement!</p>
+	<p>We are cylinder safety experts, trusted by aircraft customers all over the world. CGS upholds the most stringent standards demanded by the FAA&mdash;you can trust that your cylinders are in capable hands.</p>
+	<p>All testing is done in-house. Save money and time by bringing your cylinders to the source. Turn time is  normally 1 business day.</p>
+	<p>We offer quantity discounts starting at just three cylinders. Commercial accounts should call for competitive, negotiable rates.</p>
+	<div class="row">
+
+		<article class="col-xs-6">
+			<img src="images/scuba.jpg" alt="Self Contained Underwater Breathing Apparatus Cylinders" class="pull-right">
+			<h2><a name="scuba">SCUBA</a></h2>
+			<p class="small">CGS performs hydrostatic testing and visual inspections per DOT regulations, CGA requirements and PSI/PCI standards. Every cylinder leaves with a nominal charge of certified breathing air to ensure zero contamination and a visual inspection label that conforms to industry standards.<br/><br/>Our trained technicians can clean and label your cylinder for oxygen-enriched service for a low, fixed rate.<br/><br/>Annual inspections are only $5 and can be performed while you wait.</p>
+
+		</article>
+
+		<article class="col-xs-6">
+			<img src="images/scba.jpg" alt="SCBA Cylinders" class="pull-right">
+			<h2>SCBA</h2>
+			<p class="small">We know that Self-Contained Breathing Apparatus cylinders can take a beating. We can repair and hydro test fiber-wrapped SCBA cylinders at a fraction of the price of our competitors.</p>
+<br style="clear:both">
+<img src="images/paintball.jpg" alt="Paintball Bottles" class="pull-right">
+			<h2><a name="paintball">Paint Ball</a></h2>
+<!-- <img src="images/paintball.jpg" alt="Paintball Bottles" class="pull-right"> -->
+			<p class="small">Hydro testing your paintball cylinder doesn’t need to be headache. With low, fixed-pricing and one day turn time, there is no reason to send your cylinders elsewhere. <!-- Fill out the [form](link to form) and  -->Send us your cylinders today!</p>
+		</article>
+	</div>
+
+	<div class="row">
+
+		<article class="col-xs-6">
+			<h2><a name="medical">Medical</a></h2><img src="images/oxygen-cylinder.jpg" alt="Medical Oxygen Cylinders" class="pull-right">
+			<p class="small">Our staff is well-versed on the requirements of cylinders in oxygen service. We have the capability to quickly hydrostatically test, purge, and leak-check your oxygen cylinders for a low fixed-price. Every hydro test includes a new neck seal and external valve cleaning to ensure your safety and trouble-free operation.<br/><br/>We have a dedicated oxygen clean-room where we can safely overhaul and repair the most common oxygen valves in service today, along with the capability to clean contaminated cylinders and valves.<br/><br/>Call or <a href="contact-us.php">email</a> us for a quote.<br/><br/>Aviation customers utilizing portable oxygen systems can have bottles filled while they wait. We stock certified Aviators Breathing Oxygen and we can fill your cylinder at a substantial discount from what airport operators charge.</p>
+		</article>
+
+		<article class="col-xs-6">
+			<h2><a name="industrial">Industrial</a></h2>
+			<img src="images/cylinder.jpg" alt="Industrial" class="pull-right">
+			<p class="small">CGS has the capability to hydrostatically test almost any cylinder up to 40 inches long and 24 inches wide, with test pressures up to 10,000 PSI.<br/><br/>We can test fire extinguisher cells and cylinders, industrial and specialty gas cylinders, and even non-DOT containers for a variety of applications.<br/><br/>Feel free to stop by during normal business hours (8am-5pm PST, Monday - Friday). Or, you can send your cylinders in for service from anywhere. We ship worldwide!<br/><br/>Use the form below and include with your cylinder when sending it in. We will contact you upon receipt. No payment will be due until your cylinder is complete and the transaction can be easily completed online or over the phone.</p>
+		</article>
+
+	</div>
+
+
+<footer>
+	<!-- <p class="h5">The FAA and the EASA have approved Compressed Gas Systems to overhaul fire, oxygen, and pneumatic components for a variety of aircraft.</p> -->
+</footer>
+
+</section>
+
+<?php include 'inc/_footer.php'; ?>
