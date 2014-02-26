a17f04e571954a22dec3eb6e868eb0f35ac409d3
Alyssa Davis <alyda@me.com>
2014-02-26 00:57:30 -0800

    content: about

---
+++ about-us.php
@@ -0,0 +1,49 @@
+<?php
+$pageTitle = 'About Us';
+include 'inc/_header.php'; ?>
+
+<style>
+	.jumbotron p {font-size: 18px;}
+	.jumbotron blockquote {
+		background-color: #ececec;
+		border-radius: 6px;
+		border-left: 5px solid #00aeef;
+		font-size: 20px;
+	}
+</style>
+
+<ul class="breadcrumb">
+	<li><a href="/">home</a></li>
+	<li>about us</li>
+</ul>
+
+<!-- use .md files to edit -->
+<article class="jumbotron">
+	<h1 hidden>About Us</h1>
+
+
+
+<span><strong>Compressed Gas Systems</strong> works on your schedule. Tell us when <strong><em>you</em></strong> want it!</span>
+<br/><br/>
+<p><strong style="color:#428bca;font-size:24px;">CGS</strong> was created to offer individuals, repair organizations, and airlines an alternative to expensive OEM repair shops and exchange services for the overhaul and repair of compressed gas equipment.</p>
+
+<p>We have an on-site DOT approved hydrostatic test facility and can recertify almost any type of compressed gas cylinder used in aircraft, industrial, medical, SCUBA, SCBA, and paintball applications.</p>
+
+<p>Approved by the United States Department of Transportation (DOT), Federal Aviation Administration (FAA), and European Aviation Safety Agency (EASA), we are diligent in our conformance to industry, manufacturer, and customer standards.</p>
+
+<p>Our technicians have been trained and certified as Professional Scuba Inspectors / Professional Cylinder Inspectors and we have a certified Oxygen Cylinder Cleaning Technician in-house.  We comply with all FAA and DOT training and drug abatement programs.</p>
+
+<p>We are fully equipped to ship hazardous materials worldwide and pride ourselves on saving customers hundreds of dollars on shipping costs by being flexible and knowledgeable of regulations, rates, and fees.<p>
+
+<p>We take your safety seriously and promise the best service at the lowest cost in the least amount of time, period.</p>
+<br/>
+
+<img src="images/corporate-jet.jpg" width="640" alt="" style="display: block; margin: 0 auto; border-radius:8px;">
+<!-- <blockquote>No matter if you fly a Baron, manage a G550, or repair 747s, you have to fly.  CGS understands what it takes!</blockquote> -->
+<br/>
+<!-- <img src="images/747-endeavor.jpg" alt="" style="width:320px;margin:0 auto;display:block;"> -->
+<!-- <br/> -->
+<p class="small">CGS is approved by the Federal Aviation Administration and European Aviation Safety Agency to repair oxygen, pneumatic and fire suppression systems onboard aircraft.</p>
+
+</article>
+<?php include 'inc/_footer.php'; ?>
