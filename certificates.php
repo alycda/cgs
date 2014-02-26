f91704cfc5f3a9906472cbe37e86430d03bab71c
Alyssa Davis <alyda@me.com>
2014-02-26 00:55:19 -0800

    content: certificates

---
+++ certificates.php
@@ -0,0 +1,46 @@
+<?php
+$pageTitle = 'Certificates';
+include 'inc/_header.php'; ?>
+<ul class="breadcrumb">
+	<li><a href="/">home</a></li>
+	<li>certificates</li>
+</ul>
+
+<!-- use .md files to edit -->
+
+<section class="jumbotron">
+		<header>
+		<h1 hidden>Certificates</h1>
+		<!-- <span class="jumbo text-center">Tell us when <em>you</em> want it!</span> -->
+		<!-- <em class="h4 text-center">Specializing in Corporate and General Aviation</em> -->
+		</header>
+		<div class="row" style="border-bottom: 1px solid #ddd; padding-bottom:36px;">
+			<div class="col-xs-6 alpha">
+				<a href="pdf/cgs-air-agency-certificate.pdf"><img class="centered" src="images/air-agency-certificate.jpg" alt="Air Agency Certificate" style="border:1px solid #ccc;"></a>
+				<h2 class="text-center"><a href="pdf/cgs-air-agency-certificate.pdf">Air Agency Certificate</a></h2>
+				</div>
+			<div class="col-xs-6">
+				<a href="pdf/cgs-operations-specifications.pdf"><img class="centered" src="images/operations-specifications.jpg" alt="Operations Specifications" style="border:1px solid #ccc;"></a>
+				<h2 class="text-center"><a href="pdf/cgs-operations-specifications.pdf">Operation Specifications</a></h2>
+				</div>
+		</div>
+		<div class="row" style="padding-top:36px;">
+			<div class="col-xs-6">
+				<a href="pdf/drugcert.pdf"><img class="centered" src="images/drug-certificate.gif" alt="Drug Certificate" style="border:1px solid #ccc;"></a>
+				<h2 class="text-center"><a href="pdf/drugcert.pdf">Drug Certificate</a></h2>
+				</div>
+			<div class="col-xs-6 omega">
+				<a href="pdf/easa-certificate.pdf"><img class="centered" src="images/easa-certificate.gif" alt="EASA Certificate" style="border:1px solid #ccc;"></a>
+				<h2 class="text-center"><a href="pdf/easa-certificate.pdf">EASA Certificate</a></h2>
+				</div>
+		</div>
+		<div class="row" style="padding-top:36px;">
+			<div class="col-xs-6">
+				<a href="pdf/DOT-RIN-Approval-Letter.pdf"><img class="centered" src="images/dot-rin-certificate.gif" alt="DOT RIN Certificate" style="border:1px solid #ccc;"></a>
+				<h2 class="text-center"><a href="pdf/DOT-RIN-Approval-Letter.pdf">DOT RIN Certificate</a></h2>
+				</div>
+
+		</div>
+	</section>
+
+<?php include 'inc/_footer.php'; ?>
